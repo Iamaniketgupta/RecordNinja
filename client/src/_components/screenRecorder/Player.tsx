fb6591e03a62e96d8a2acd9398a39604c27dabb9
Aniket Gupta <aniketgupta.org@gmail.com>
2024-12-01 02:11:11 +0530

    fix responsiveness

--- client/src/_components/screenRecorder/Player.tsx
+++ client/src/_components/screenRecorder/Player.tsx
@@ -13,11 +13,11 @@ export default function Player({ stream ,setStream}: { stream: MediaStream | nul
         <>
             {
                 stream ?
-                    <video ref={videoRef} className='object-contain w-full  h-full rounded-md' autoPlay muted controls={false}>Player</video>
+                    <video ref={videoRef} className='object-contain w-full   h-full rounded-md' autoPlay muted controls={false}>Player</video>
                     : <div className='w-full h-full flex items-center justify-center bg-gradient-to-r from-indigo-500 to-stone-900'>
                         <div>
 
-                            <p className='text-white text-xl'>Video Stream Not Found. Please ensure you have access to camera and microphone.</p>
+                            <p className='text-white md:text-xl'>Video Stream Not Found. Please ensure you have access to camera and microphone.</p>
                             <div 
                             onClick={()=>getScreenStream().then(stream=>setStream(stream))}
                             className='bg-gradient-to-r from-indigo-500 to-pink-600 hover:from-pink-600 hover:to-indigo-500 w-fit px-3 py-2 my-4 text-white cursor-pointer rounded mx-auto flex items-center justify-center'>
--- client/src/app/record/page.tsx
+++ client/src/app/record/page.tsx
@@ -42,18 +42,18 @@ export default function Page() {
 
                 {/* Video Area*/}
 
-                {!downloadUrl && <div className='m-5 rounded-xl h-[70%] mx-auto max-w-4xl bg-gradient-to-r from-indigo-500 to-stone-900 overflow-clip p-2 '>
+                {!downloadUrl && <div className='m-5 max-sm:mx-3 rounded-xl h-[70%] mx-auto max-w-4xl bg-gradient-to-r from-indigo-500 to-stone-900 overflow-clip p-2 '>
                     <Player setStream={setStream} stream={stream} />
                 </div>}
 
                 {/* Download area */}
                 {downloadUrl &&
-                    <div className='grid grid-cols-4 gap-4 rounded-3xl p-4 m-5'>
-                        <div className='w-[80%] col-span-3 my-4 h-auto min-h-fit'>
+                    <div className='flex gap-5 flex-wrap rounded-3xl md:p-4 m-5'>
+                        <div className='max-w-3xl flex-grow my-4 h-auto min-h-fit'>
                             <video src={downloadUrl} autoPlay={true} controls />
                         </div>
 
-                        <div className='w-full min-h-full flex flex-col gap-4 items-center justify-center'>
+                        <div className='w-full min-h-full flex flex-col max-w-xl mx-auto gap-4 items-center justify-center'>
                             <a href={downloadUrl} download='recording.mp4' 
                             className=' w-full text-center rounded-lg 
                             hover:bg-gradient-to-r hover:to-indigo-500 hover:from-blue-600 px-8 py-3 bg-gradient-to-r from-indigo-700 to-blue-600 hover:bg-indigo-400 text-white text-sm font-semibold'>
@@ -74,7 +74,7 @@ export default function Page() {
 
                 {/* Toolbar */}
                 {
-                    !downloadUrl && <div className='h-16 bg-white  p-4 dark:bg-stone-800 m-4 rounded-xl max-w-4xl mx-auto text-5xl  shadow-3xl'>
+                    !downloadUrl && <div className='h-16 bg-white max-sm:mx-4 p-4 dark:bg-stone-800 m-4 rounded-xl max-w-4xl mx-auto text-5xl  shadow-3xl'>
                         <Tools
                             setDownloadUrl={setDownloadUrl}
                             stream={stream} audioMuted={audioMuted}
